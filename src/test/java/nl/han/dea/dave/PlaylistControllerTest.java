ed8a7e7d38c380032810ff7d8a6fa0b40cc9903c
unknown <nikerts2@gmail.com>
2018-10-12 17:28:07 +0200

    Unittests Playlistcontroller

--- src/test/java/nl/han/dea/dave/PlaylistControllerTest.java
+++ src/test/java/nl/han/dea/dave/PlaylistControllerTest.java
@@ -1,12 +1,9 @@
 package nl.han.dea.dave;
 
-import nl.han.dea.dave.controllers.LoginController;
 import nl.han.dea.dave.controllers.PlaylistController;
-import nl.han.dea.dave.controllers.dto.PlaylistDTO;
-import nl.han.dea.dave.controllers.dto.PlaylistRequestDTO;
-import nl.han.dea.dave.controllers.dto.PlaylistsDTO;
-import nl.han.dea.dave.controllers.dto.UserResponseDTO;
+import nl.han.dea.dave.controllers.dto.*;
 import nl.han.dea.dave.services.PlaylistService;
+import nl.han.dea.dave.services.TrackService;
 import nl.han.dea.dave.services.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,44 +16,49 @@ import java.util.ArrayList;
 public class PlaylistControllerTest {
 
     private final String TOKEN = "1234-1234-1234";
-    private final int USERID = 1;
     private final int PLAYLISTID = 1;
+    private final int TRACKID = 1;
 
-    private PlaylistService playlistService;
     private PlaylistController playlistController;
-    private PlaylistsDTO playlistsDTO;
     private PlaylistRequestDTO playlistRequestDTO;
-    private UserService userService;
 
     @BeforeEach
     public void setup() {
         // playlist
         playlistController = new PlaylistController();
-        playlistService = Mockito.mock(PlaylistService.class);
+        PlaylistService playlistService = Mockito.mock(PlaylistService.class);
         playlistController.setPlaylistService(playlistService);
         playlistRequestDTO = new PlaylistRequestDTO();
 
         // userService
-        userService = Mockito.mock(UserService.class);
+        UserService userService = Mockito.mock(UserService.class);
         playlistController.setUserService(userService);
+
+        // trackService
+        TrackService trackService = Mockito.mock(TrackService.class);
+        playlistController.setTrackService(trackService);
 
         // All playlists
         ArrayList<PlaylistDTO> playLists = new ArrayList<>();
         playLists.add(new PlaylistDTO());
-        playlistsDTO = new PlaylistsDTO(playLists);
+        PlaylistsDTO playlistsDTO = new PlaylistsDTO(playLists);
 
         // MOCKITO
         // control token
         Mockito.when(userService.tokenIsValid(TOKEN)).thenReturn(true);
         // get userId
-        Mockito.when(userService.getUserIdFromToken(TOKEN)).thenReturn(USERID);
+        int userId = 1;
+        Mockito.when(userService.getUserIdFromToken(TOKEN)).thenReturn(userId);
         // get all plalists from user
-        Mockito.when(playlistService.allPlaylistsFromUser(USERID)).thenReturn(playlistsDTO);
+        Mockito.when(playlistService.allPlaylistsFromUser(userId)).thenReturn(playlistsDTO);
         // PlaylistExists
         Mockito.when(playlistService.playlistExist(PLAYLISTID)).thenReturn(true);
-        // get all plalists from user
-        Mockito.when(playlistService.allPlaylistsFromUser(USERID)).thenReturn(playlistsDTO);
-
+        // Check if track exists
+        Mockito.when(trackService.trackExists(TRACKID)).thenReturn(true);
+        // OfflineAvaliableIsNotTheSame
+        Mockito.when(trackService.offlineAvailableIsTheSame(TRACKID, true)).thenReturn(false);
+        // Track doesn't exists in playlist yet
+        Mockito.when(trackService.trackExistsInPlaylist(PLAYLISTID, TRACKID)).thenReturn(false);
     }
 
     @Test
@@ -127,6 +129,25 @@ public class PlaylistControllerTest {
         Response test = playlistController.setTracks(2, TOKEN);
 
         Assertions.assertEquals(401, test.getStatus());
+    }
+
+    @Test
+    public void checkIfAddTracksToPlaylistReturns200(){
+        //TrackDTO
+        TrackDTO trackDTO = new TrackDTO();
+        trackDTO.setId(TRACKID);
+        trackDTO.setOfflineAvailable(true);
+
+        Response test = playlistController.addTrackToPlaylist(PLAYLISTID, TOKEN, trackDTO);
+
+        Assertions.assertEquals(200, test.getStatus());
+    }
+
+    @Test
+    public void checkDeleteTrackFromPlaylistReturns200(){
+        Response test = playlistController.deleteTrackFromPlaylist(PLAYLISTID, TRACKID, TOKEN);
+
+        Assertions.assertEquals(200, test.getStatus());
     }
 
 }
